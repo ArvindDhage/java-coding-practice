56ef23f89b363875cad6a1b61b790b3f09c1994c
Arvind Dhage <arvinddhage35@gmail.com>
2025-08-08 22:16:29 +0530

    Area of rectangle

---
+++ asg3_p2.java
@@ -0,0 +1,16 @@
+// Area Of Triangle
+import java.util.Scanner;
+public class asg3_p2 {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        float area;
+        System.out.println("Enter a Length");
+        int l=sc.nextInt();
+        System.out.println("Enter a bridth ");
+        int b=sc.nextInt();
+        area=l*b;
+        System.out.println("Area of Rectangle is "+area);
+        
+    }
+    
+}
