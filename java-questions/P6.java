8e4de124745556b98863bccdc68f9aafc44394a5
Arvind dhage <arvinddhage35@gmail.com>
2025-11-22 08:38:21 +0530

    Problem 6:Area of Rectangle

---
+++ java-questions/P6.java
@@ -0,0 +1,14 @@
+// Problem 6:Area of Rectangle
+import java.util.Scanner;
+public class P6 {
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter the length of trangle ");
+    double l=sc.nextDouble();
+    System.out.print("Enter the brith of trangle");
+    double b=sc.nextDouble();
+    double area=l*b;
+    System.out.print("Area of trangle is :"+area);
+}
+    
+}
