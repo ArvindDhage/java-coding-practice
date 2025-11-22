be88e5d74e5d874d25939f44d22cf259b09907a0
Arvind dhage <arvinddhage35@gmail.com>
2025-11-22 08:30:38 +0530

    Problem 5:Area of Rhombus

---
+++ java-questions/P5.java
@@ -0,0 +1,14 @@
+// Problem 5:Area of Rhombus
+import java.util.Scanner;
+public class P5 {
+         public static void main(String args[]){
+         Scanner sc=new Scanner(System.in);
+         System.out.print("Enter the p Diagonal of rhombus :");
+         double p=sc.nextDouble();
+         System.out.print("Enter the q Diagonal of rhombus :");
+         double q=sc.nextDouble();
+         double area =(p*q)/2;
+         System.out.println("Area of Rhombus is :"+area);
+}
+    
+}
