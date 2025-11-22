9b721f81ebaadcfe2bc4d89889dd884eb85392a1
Arvind dhage <arvinddhage35@gmail.com>
2025-11-22 08:52:30 +0530

    Problem 7:Area of Equilateral trangle

---
+++ java-questions/P7.java
@@ -0,0 +1,15 @@
+//Problem 7:Area of Equilateral trangle 
+import java.util.Scanner;
+public class P7 {
+   public static void main(String args[]){
+   Scanner sc=new Scanner(System.in);
+   System.out.print("Enter the said of  Equilateral trangle ");
+   double s=sc.nextDouble();
+   double area=(Math.sqrt(3)/4)*(s*s);
+   System.out.print("Area of Equilateral trangle : "+area);
+
+   
+   }
+
+    
+}
