8d69b1807e7d6efff229e325eb47770e2e63b578
Arvind dhage <arvinddhage35@gmail.com>
2025-11-22 09:09:54 +0530

    Problem 9 : Area of Eqilateral trangle

---
+++ java-questions/P9.java
@@ -0,0 +1,12 @@
+//Problem 9 : Area of Eqilateral trangle 
+import java.util.Scanner;
+public class P9 {
+    public static void main(String args[]){
+    Scanner sc=new Scanner (System.in);
+    System.out.print("Enter the side of Eqilateral trangle ");
+    double a=sc.nextDouble();
+    double area=3*a;
+    System.out.print("Area of Eqilateral trangle"+area);
+}
+    
+}
