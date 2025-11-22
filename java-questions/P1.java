0d6697e2a31093f650d75b81982945924bc6b8df
Arvind dhage <arvinddhage35@gmail.com>
2025-11-22 07:26:39 +0530

    Problem 1:Area of Circle

--- java-questions/P1.java
+++ java-questions/P1.java
@@ -1,1 +1,21 @@
-#Problem 1:Area of circle in java programming 
+//Problem 1:Area of circle in java programming 
+
+import java.util.Scanner;
+
+public class P1 {
+
+    public static void main (String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the radious of circele");
+        float r=sc.nextFloat();
+        float area;
+        float pi=3.14f;
+        area=pi*r*r;
+        System.out.println("Area of circe is "+area);
+
+        
+
+        
+    }
+}
+
