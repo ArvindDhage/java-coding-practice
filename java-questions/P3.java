84dce63256d45a5af8002a277a9696b2c4310ac2
Arvind dhage <arvinddhage35@gmail.com>
2025-11-22 08:14:49 +0530

    Problem 3:Area of  Isosceles Trangle

---
+++ java-questions/P3.java
@@ -0,0 +1,16 @@
+// Problem 3 :Area of  Isosceles Trangle 
+import java.util.Scanner;
+public class P3 {
+   public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the hight of Isosceles trangle");
+        double hight=sc.nextDouble();
+        System.out.println("Enter the base of Isosceles trangle");
+        double base=sc.nextDouble();
+        double area=(1*hight*base)/2;
+        System.out.println("Area of Isosceles trangle is :"+area);
+
+    
+    
+}
+}
