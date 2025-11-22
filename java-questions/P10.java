48f1227ff3e13c8aa2fadba2cc11cf3407726836
Arvind dhage <arvinddhage35@gmail.com>
2025-11-22 09:18:50 +0530

    Problem 10 : Parameter of Parallelogram

---
+++ java-questions/P10.java
@@ -0,0 +1,15 @@
+// Problem 10 :Parameter of Parallelogram 
+import java.util.Scanner;
+public class P10 {
+   public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Base of Parallelogram :");
+        double s=sc.nextDouble();
+        System.out.print("Enter the base of Parallelogram :");
+        double b=sc.nextDouble();
+        double area =2*(s+b);
+        System.out.print(" Area of Parallelogram is :" +area);
+}
+ 
+    
+}
