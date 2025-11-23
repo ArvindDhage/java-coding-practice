833aa25aa071295fda89e9ca49206c7ea358ec1f
Arvind dhage <arvinddhage35@gmail.com>
2025-11-23 07:09:02 +0530

    Problem 13 : Perimeter of rhombus

---
+++ java-questions/P13.java
@@ -0,0 +1,12 @@
+//Problem 13 : Perimeter of rhombus
+import java .util.Scanner;
+public class P13 {
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter the said of rhombus :");
+    double a=sc.nextDouble();
+    double p=4*a;
+    System.out.print("Perimeter of rhombus is :"+p);
+}
+    
+}
