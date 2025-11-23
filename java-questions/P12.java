714708975d336f47fbc18e600df9f1dfece8df80
Arvind dhage <arvinddhage35@gmail.com>
2025-11-23 07:00:35 +0530

    Problem 12 : Perimeter of square

---
+++ java-questions/P12.java
@@ -0,0 +1,13 @@
+//Problem 12 :Perimeter of square 
+import java.util.Scanner;
+
+public class P12 {
+     public static void main(String args[]){
+     Scanner sc=new Scanner (System.in);
+     System.out.print("Entere the said of square :");
+     double a=sc.nextDouble();
+     double p=4*a;
+     System.out.print("Perimeter of square is :"+p);
+}
+    
+}
