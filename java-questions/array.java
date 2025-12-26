538032f31fc781a0334ea0ca39c147d348d8d58c
Arvind dhage <arvinddhage35@gmail.com>
2025-12-26 14:06:28 +0530

    Array in java

---
+++ java-questions/array.java
@@ -0,0 +1,9 @@
+public class array{
+    public static void main(String [] args){
+        int a[]={1,2,3,4};
+       // a[1]=10;// updating value of index 1
+       for(int i=0;i<4;i++){
+        System.out.print(a[i]);
+       }
+    }
+}
